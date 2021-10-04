8cfb5a0345cc9e18b4436b3456ae648ae44d7aac
haris18896 <haris18896@gmail.com>
2021-10-04 00:28:36 -0700

    working with navbar component

--- crypto_currency/src/App.js
+++ crypto_currency/src/App.js
@@ -1,15 +1,3 @@
-# CryptoCurrencyAppReactRedux
-
-```sh
-npm install antd @ant-design/icons react-redux @reduxjs/toolkit axios chart.js html-react-parser millify moment react-chartjs-2
-
-npm install firebase
-npm i react-router-dom
-```
-
-we will be using `Rapid API` for this build and also `Bing News Search API` from Rapid API which will allow us to fetch crypto currency related news.
-
-```js
 import React from 'react';
 import './App.css';
 import { Switch, Route, Link } from 'react-router-dom';
@@ -34,5 +22,3 @@ function App() {
 }
 
 export default App;
-```
-
