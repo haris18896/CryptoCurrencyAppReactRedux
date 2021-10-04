513f187dbcf5632ccfd5b156c4881e9bd982e881
haris18896 <haris18896@gmail.com>
2021-10-04 05:43:07 -0700

    homepage statistics done

--- crypto_currency/src/components/homepage/HomePage.jsx
+++ crypto_currency/src/components/homepage/HomePage.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import millify from 'millify';
+import { Typography, Row, Col, Statistic} from 'antd';
+
+
+const { Title } = Typography;
 
 function HomePage() {
     return (
-        <div>
-            <h1>HomePage</h1>
-        </div>
+        <>
+            <Title level={2} className="heading">Global Crypto Status</Title>
+            <Row>
+                <Col span={12}><Statistic title="Total Crypto Currencies" value="5" /></Col>
+                <Col span={12}><Statistic title="Total Exchanges" value="5" /></Col>
+                <Col span={12}><Statistic title="Total Market Cap" value="5" /></Col>
+                <Col span={12}><Statistic title="Total 24th Volume" value="5" /></Col>
+                <Col span={12}><Statistic title="Total Markets" value="5" /></Col>
+            </Row>
+        </>
     )
 }
 
