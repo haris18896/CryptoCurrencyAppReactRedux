f95ba042a0859b909d03cf98d3d542ee932d1ab2
haris18896 <haris18896@gmail.com>
2021-10-05 00:45:32 -0700

    firebase

---
+++ crypto_currency/src/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDQZR6s5fHzRDSySL2Cqmy-rGdCRcBuZHg",
+    authDomain: "cryptoverse-03.firebaseapp.com",
+    projectId: "cryptoverse-03",
+    storageBucket: "cryptoverse-03.appspot.com",
+    messagingSenderId: "474697413981",
+    appId: "1:474697413981:web:6f78cbdd1672cceffb24b4"
+  };
+
+  firebase.initializeApp(firebaseConfig);
