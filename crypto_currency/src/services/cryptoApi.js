751a5b94fac9cf93ad73f7a1a91adc3c8ecbedf7
haris18896 <haris18896@gmail.com>
2021-10-05 00:01:35 -0700

    chart added to the page

--- crypto_currency/src/services/cryptoApi.js
+++ crypto_currency/src/services/cryptoApi.js
@@ -14,22 +14,21 @@ export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery: fetchBaseQuery({ baseUrl }),
     endpoints: (builder) => ({
-        getCryptos:  builder.query({
-            query : (count) => createRequest(`/coins?limit=${count}`),  
-        }),
-        getCryptoDetails:  builder.query({
-            query : (coinId) => createRequest(`/coin/${coinId}`),  
-        }),
-        getCryptoHistory:  builder.query({
-            query : (coinId, timePeriod) => createRequest(`/coin/${coinId}/history/${timePeriod}`),  
-        }),
+      getCryptos: builder.query({
+        query: (count) => createRequest(`/coins?limit=${count}`),
+      }),
+      getExchanges: builder.query({
+        query: () => createRequest('/exchanges'),
+      }),
+      getCryptoDetails: builder.query({
+        query: (coinId) => createRequest(`/coin/${coinId}`),
+      }),
+      getCryptoHistory: builder.query({
+        query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history/${timeperiod}`),
+      }),
     }),
-});
-
-export const {
-    useGetCryptosQuery,
-    useGetCryptoDetailsQuery,
-    useGetCryptoHistoryQuery,
-} = cryptoApi;
+  });
+  
+  export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetExchangesQuery, useGetCryptoHistoryQuery } = cryptoApi;
 
 
