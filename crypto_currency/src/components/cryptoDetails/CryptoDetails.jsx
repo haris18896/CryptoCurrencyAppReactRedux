16ba7f8ee266c03c42b3c731fdeba9cdd2c4ab4e
haris18896 <haris18896@gmail.com>
2021-10-04 20:03:14 -0700

    working with coin details page

--- crypto_currency/src/components/cryptoDetails/CryptoDetails.jsx
+++ crypto_currency/src/components/cryptoDetails/CryptoDetails.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import React from 'react';
+import HTMLReactParser from 'html-react-parser';
+import { useParams } from 'react-router-dom';
+import millify from 'millify';
+import { Row, Col, Select, Typography } from 'antd';
+import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined,
+    TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
+
 
 function CryptoDetails() {
     return (
